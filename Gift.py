9572e4d1459496f0e256c62a351f4a8c6e170ac7
chosenjadon-netizen <chosenjadon@gmail.com>
2026-04-25 13:45:05 +0530

    Add files via upload

---
+++ Gift.py
@@ -0,0 +1,20 @@
+print("Hello and welcome to a birthday center !")
+age = int(input("What is Your Age \n"))
+if age < 12:
+    print("Your at the below of the 12 age")
+elif age < 18:
+    print("Your at the below of the 18 age")
+else:
+    print("Your at the over of the 18 age")
+
+sport = input("What activities you want to do ? \n")
+if sport != "sport":
+    print("Sure anything is fine :D")
+
+restaurant = input("Is there any places you want to go ? \n")
+if restaurant != "Yes":
+    print("Its your choice :>")
+
+
+    print("And Happy Birthday Have a good time")
+
